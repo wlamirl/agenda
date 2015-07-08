474a85fc35f31d8ebe42e6a8e31d7e091d78425b
wlamirl <wlamirl@VMUPsmnWL>
2015-07-07 23:41:21 -0300

    Segundo commit

---
+++ module/Contato/src/Contato/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Contato\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class HomeController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
